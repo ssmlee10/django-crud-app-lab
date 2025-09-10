c773396acfc80a24df71964b611251a2ba505ef2
Sharon Lee <ssmlee10@gmail.com>
2025-09-10 12:57:51 -0700

    add views, example movies

--- main_app/views.py
+++ main_app/views.py
@@ -3,12 +3,26 @@ from django.shortcuts import render
 # import HttpResonse to send text-based responses
 from django.http import HttpResponse
 
+class Movie:
+  def __init__(self, title, director, year, genre, rating):
+    self.title = title
+    self.director = director
+    self.year = year
+    self.genre = genre
+    self.rating = rating
+
+movies = [
+  Movie('Kpop Demon Hunters', 'Maggie Kang', 2025, 'Animation/ Action', 10.0),
+  Movie("Parasite", "Bong Joon-ho", 2019, "Thriller/Drama", 8.6),
+  Movie("Train to Busan", "Yeon Sang-ho", 2016, "Action/Horror", 7.6),
+]
+
 def home(request):
   return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
-# def movies_index(request):
-#   movies = Movie.object.all()
-#   return render(request, 'movies/index.html', {'movies': movies})
+def movies_index(request):
+  movies = Movie.object.all()
+  return render(request, 'movies/index.html', {'movies': movies})
