1ea37ae97deec1719ebbd575b35453bf7bde12c0
Sharon Lee <ssmlee10@gmail.com>
2025-09-10 12:51:25 -0700

    add movies page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  # path('movies/', views.movie_index, name='movie-index'),
+  path('movies/', views.movie_index, name='movie-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse('<h1>Hello, this is my movie app!</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
