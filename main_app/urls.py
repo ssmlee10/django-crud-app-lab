7e8e7e1e8af06db3f3b9c2dc0444b531959adbb1
Sharon Lee <ssmlee10@gmail.com>
2025-09-11 13:57:11 -0700

    add edit, delete buttons

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
   path('movies/', views.movie_index, name='movie-index'),
   path('movies/<int:movie_id>/', views.movie_detail, name='movie-detail'),
   path('movies/create/', views.MovieCreate.as_view(), name='movie-create'),
+  path('movies/<int:pk>/update/', views.MovieUpdate.as_view(), name='movie-update'),
+  path('movies/<int:pk>/delete', views.MovieDelete.as_view(), name='movie-delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Movie
 
 # import HttpResonse to send text-based responses
@@ -23,3 +23,11 @@ def movie_detail(request, movie_id):
 class MovieCreate(CreateView):
   model = Movie
   fields = '__all__'
+
+class MovieUpdate(UpdateView):
+  model = Movie
+  field = '__all__'
+
+class MovieDelete(DeleteView):
+  model = Movie
+  success_url = '/movies/'
