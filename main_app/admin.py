f1d38a2d371b012245c1e7860de48d993fada65d
Sharon Lee <ssmlee10@gmail.com>
2025-09-12 12:35:09 -0700

    add actors to the admin portal

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Actor
 
 # Register your models here.
 # this brings in the movie model into the admin site
 admin.site.register(Movie)
+admin.site.register(Actor)
--- main_app/models.py
+++ main_app/models.py
@@ -40,7 +40,6 @@ class Movie(models.Model):
   
 class Actor(models.Model):
   name = models.CharField(max_length=50)
-  birthday = models.DateField('Birth Date')
   role = models.CharField(max_length=100)
 
   movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
